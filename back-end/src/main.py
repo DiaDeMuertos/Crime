8dad8144b9584df713b616057f0c6fbd58d78fdc
DiaDeMuertos <imworkingnow@msn.com>
2019-11-03 16:07:59 -0700

    added crimes_sample

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -23,7 +23,7 @@ def close_conection(cursor, connection):
         connection.close()
 
 
-def build_crimes_by(row):
+def build_crimes(row):
     return {
         "id": int(row[0]),
         "geojson": json.loads(row[1])
@@ -36,6 +36,43 @@ def build_grid(row):
         "geojson": json.loads(row[1])
     }
 
+
+@app.route('/api/crimes/sample', methods=['GET'])
+def crimes_sample():
+    if request.method == 'GET':
+        try:
+            connection = psycopg2.connect(
+                user="postgres",
+                password="123",
+                host="db",
+                port="5432",
+                database="crimes",
+            )
+
+            cursor = connection.cursor()
+
+            cursor.execute("""
+                SELECT 
+                    id,
+                    ST_ASGEOJSON(geom) AS geojson
+                FROM crimes
+                LIMIT 100;
+            """)
+
+            matches = True if cursor.rowcount != 0 else False
+        
+            payload = [build_crimes(r) for r in cursor]
+            success = True
+            code = 200
+        except(Exception, psycopg2.Error) as error:
+            print(error)
+
+            payload = None
+            success = False
+            code = 400
+        finally:
+            close_conection(cursor, connection)
+            return jsonify({"payload": payload, "success": success, "matches": matches}), code
 
 @app.route('/api/grid', methods=['GET'])
 def grid():
@@ -72,8 +109,8 @@ def grid():
             return jsonify({"payload": payload, "success": success, "matches": matches}), code
 
 
-@app.route('/api/crimes/by/<group_id>/<hour>/<day_of_week>/<month>/<tract_id>/<grid_id>', methods=['GET'])
-def crimes_by(group_id, hour, day_of_week, month, tract_id, grid_id):
+@app.route('/api/crimes/by/<group_id>/<hour>/<day_of_week>/<month>/<grid_id>', methods=['GET'])
+def crimes_by(group_id, hour, day_of_week, month, grid_id):
     if request.method == 'GET':
         try:
             connection = psycopg2.connect(
@@ -96,9 +133,8 @@ def crimes_by(group_id, hour, day_of_week, month, tract_id, grid_id):
                     hour= %s AND 
                     day_of_wee = %s AND 
                     month = %s AND 
-                    tract_id = %s AND 
                     grid_id = %s;
-            """ % (group_id, hour, day_of_week, month, tract_id, grid_id))
+            """ % (group_id, hour, day_of_week, month, grid_id))
 
             matches = True if cursor.rowcount != 0 else False
 
@@ -112,7 +148,7 @@ def crimes_by(group_id, hour, day_of_week, month, tract_id, grid_id):
                         group_id = %s AND grid_id = %s;
                 """ % (group_id, grid_id))
 
-            payload = [build_crimes_by(r) for r in cursor]
+            payload = [build_crimes(r) for r in cursor]
             success = True
             code = 200
         except(Exception, psycopg2.Error) as error:
@@ -126,12 +162,11 @@ def crimes_by(group_id, hour, day_of_week, month, tract_id, grid_id):
             return jsonify({"payload": payload, "success": success, "matches": matches}), code
 
 
-@app.route('/api/crimes/predict/<hour>/<day_of_week>/<month>/<tract_id>/<grid_id>', methods=['GET'])
-def crimes_predict(hour, day_of_week, month, tract_id, grid_id):
+@app.route('/api/crimes/predict/<hour>/<day_of_week>/<month>/<grid_id>', methods=['GET'])
+def crimes_predict(hour, day_of_week, month, grid_id):
     if request.method == 'GET':
         try:
-            prediction = int(model.predict(
-                [[hour, day_of_week, month, tract_id, grid_id]])[0])
+            prediction = int(itertoolz.first(model.predict([[hour, day_of_week, month, grid_id]])))
             payload = {"group_id": prediction}
             success = True
             code = 200
@@ -152,20 +187,17 @@ if __name__ == '__main__':
     crimes_dir_exists = path.exists(path.join(path_one_step_back, 'csv-files'))
 
     if crimes_dir_exists:
-        df = pd.read_csv(path.join(path_one_step_back,
-                                   'csv-files', 'crimes.csv'))
+        df = pd.read_csv(path.join(path_one_step_back, 'csv-files', 'crimes.csv'))
     else:
-        df = pd.read_csv(path.join(path_two_step_back,
-                                   'csv-files', 'crimes.csv'))
+        df = pd.read_csv(path.join(path_two_step_back, 'csv-files', 'crimes.csv'))
 
     group_ids = [57, 20, 41, 34, 36, 19, 42]
-    columns_to_drop = ['id', 'offense_code',
-                       'offense_group', 'longitude', 'latitude', 'year']
+    columns_to_drop = ['id', 'offense_code', 'offense_group', 'longitude', 'latitude', 'year', 'tract_id']
 
     df = df[df['group_id'].isin(group_ids)].copy()
     df.drop(columns_to_drop, axis=1, inplace=True)
 
-    X = df[['hour', 'day_of_week', 'month', 'tract_id', 'grid_id']]
+    X = df[['hour', 'day_of_week', 'month', 'grid_id']]
     y = df['group_id']
 
     X_train, X_test, y_train, y_test = train_test_split(
